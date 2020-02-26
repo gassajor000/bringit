8bb72775b4951567d9b4bb06420f828b0b7a4152
MARAUDERSMAP\gassa <lgassjsg@gmail.com>
2020-02-25 23:04:59 -0800

    Add google charts api for points graph.

---
+++ public/js/points-jq.js
@@ -0,0 +1,35 @@
+'use strict';
+
+// Call this function when the page loads (the "ready" event)
+$(document).ready(function() {
+	initializePage();
+})
+
+function initializePage(){
+    google.charts.load("current", {packages:["corechart"]});
+    google.charts.setOnLoadCallback(drawChart);
+}
+
+function getFirstName(name){
+    var names = name.split(' ');
+    return names == "" ? names : names[0];
+}
+
+
+function drawChart() {
+    var pointsData = $('#pointsData').data('points');
+    var pointsArray = pointsData.map(entry => [getFirstName(entry.name), entry.points]);
+    pointsArray.unshift(['Guest', 'Points']);
+    var data = google.visualization.arrayToDataTable(pointsArray);
+
+    var options = {
+        pieHole: 0.5,
+        // legend: {position:'top', maxLines:3},
+        legend: 'none',
+        pieSliceText: 'label',
+        // pieSliceTextStyle: {color: 'black'},
+    };
+
+    var chart = new google.visualization.PieChart($('#donutchart')[0]);
+    chart.draw(data, options);
+}
